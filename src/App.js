2063b43ba01ab1e9eb694c5ff97f95be4fc9a9b4
MarcusMadMax <marcuszillner@icloud.com>
2020-08-02 21:53:48 +1200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="wrap">
+          <div className="container">
+            <div className="todos">
+              <form action="#">
+                <div className="formGroup newToDo">
+                  <label for="newToDo"></label>
+                  <input type="text" id="newToDo" placeholder="Add" />
+                </div>
+                <div className="formGroup status">
+                  <label for="status"></label>
+                  <select name="status" id="status">
+                    <option value="status">please select</option>
+                    <option value=""></option>
+                  </select>
+                </div>
+                <button type="submit" className="button">Add</button>
+              </form>
+              <div className="todo">
+                <div className="container">
+                  <i className="far fa-trash-alt"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
