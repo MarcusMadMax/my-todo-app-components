c7375c538f725669b38772fa04690409548de98c
MarcusMadMax <marcuszillner@icloud.com>
2020-08-03 09:59:11 +1200

    bla

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { Component } from 'react';
 
 class App extends Component {
   constructor(props) {
@@ -12,28 +11,25 @@ class App extends Component {
     return (
       <div className="App">
         <div className="wrap">
-          <div className="container">
-            <div className="todos">
-              <form action="#">
-                <div className="formGroup newToDo">
-                  <label for="newToDo"></label>
-                  <input type="text" id="newToDo" placeholder="Add" />
-                </div>
-                <div className="formGroup status">
-                  <label for="status"></label>
-                  <select name="status" id="status">
-                    <option value="status">please select</option>
-                    <option value=""></option>
-                  </select>
-                </div>
-                <button type="submit" className="button">Add</button>
-              </form>
-              <div className="todo">
-                <div className="container">
-                  <i className="far fa-trash-alt"></i>
-                </div>
+          <div className="container todos">
+            <form action="#">
+              <div className="formGroup newToDo">
+                <label htmlFor="newToDo"></label>
+                <input type="text" id="newToDo" placeholder="Add" />
               </div>
-            </div>
+              <div className="formGroup status">
+                <label htmlFor="status"></label>
+                <select name="status" id="status">
+                  <option value="status">please select</option>
+                  <option value=""></option>
+                </select>
+              </div>
+              <button type="submit" className="button">Add</button>
+            </form>
+          </div>
+          <div className="container todo">
+            <h1>Go surfing</h1>
+            <i className="far fa-trash-alt"></i>
           </div>
         </div>
       </div>
